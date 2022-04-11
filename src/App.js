8fb52a3eda3bee86bd4dbc2523a81416b5b4ee3f
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-11 20:56:32 +0530

    2 components Navbar Home

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import About from './Components/About';
+import Home from './Components/Home';
+
+
+
+function App() {
+  return (
+    <>
+    <Home />
+    <About />
+    </>
+  );
+}
+
+export default App;
