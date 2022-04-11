97da85b5de8651232c40fdc440e8b0e0601742fa
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-11 23:18:44 +0530

    Completed

---
+++ src/Components/Services.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Equipments from './Scomponents/Equipments'
+import Testimonial from './Scomponents/Testimonial'
+
+const Services = () => {
+  return (
+    <>
+    <Equipments />
+    <Testimonial />
+    </>
+  )
+}
+
+export default Services
