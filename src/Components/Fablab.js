ffc87ee0c6969ae970c159f0bee1def751789399
NIKHIL TAPARIA <76565229+taparia11@users.noreply.github.com>
2022-04-21 01:27:25 +0530

    main website

---
+++ src/Components/Fablab.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import About from './About';
+import Home from './Home';
+import Pricing from './Pricing';
+import Services from './Services';
+import Faq from './Faq';
+import Team from './Team';
+import Contact from './Contact';
+import Footer from './Footer';
+const Fablab = () => {
+  return (
+    <>
+    <Home />
+    <About />
+    <Services />
+    <Pricing />
+    <Faq />
+    <Team />
+    <Contact />
+    <Footer />
+    </>
+  )
+}
+
+export default Fablab
